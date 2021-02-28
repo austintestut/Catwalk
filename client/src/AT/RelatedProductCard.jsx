8d5880ec905ce35d490fbe50a7ab32441aa82c8f
Austin Testut <austintestut@gmail.com>
2021-02-28 15:15:11 -0600

    included .js file extension for CircleCI to find properly

--- client/src/AT/RelatedProductCard.jsx
+++ client/src/AT/RelatedProductCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import TOKEN from '../../../config';
+import TOKEN from '../../../config.js';
 
 class RelatedProductCard extends React.Component {
   constructor(props) {
