bb1edcafb6c4f4c56f627199218533f86eb537a1
AlexShold <ashold12@gmail.com>
2021-03-01 19:46:45 -0600

    partial stars imported --> need to render

--- client/src/components/reviews_src/StarStatic.jsx
+++ client/src/components/reviews_src/StarStatic.jsx
@@ -1,14 +1,24 @@
 // eslint-disable no-eval
 import React from 'react';
+import quarter_star from '../../../../images/quarter_star.png';
+import three_quarter_star from '../../../../images/three_quarter_star.png';
 // import star from '../../../../images/star-empty.png'
 
 const StarStatic = ({ number }) => {
   // const emptyStar = <i class="far fa-star"/>;
   // const solidStar = <i class="fas fa-star"/>;
+  // const halfStar = <i class="fas fa-star-half-alt"></i>
+  let style = {
+    width: '18px',
+    verticalAlign: 'bottom',
+  }
+  const quarterStar = <img src={quarter_star} style = {{...style}}/>;
+  const threeQuarterStar = <img src={three_quarter_star} style = {{...style}}/>
+
   let decrementer = number;
   const makeStar = () => {
-    if (decrementer > 0) {decrementer--; return <i class="fas fa-star"/>; }
-    return <i class="far fa-star"/>;
+    if (decrementer > 0) {decrementer--; return <i class="fas fa-star" style = {{...style}} />; }
+    return <i class="far fa-star" style = {{...style}} />;
   };
   const makeStars = () => {
     let stars = [];
