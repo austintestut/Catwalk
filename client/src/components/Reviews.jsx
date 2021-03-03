aec8b02b9fec281f8e4803850019960f3eb910a3
AlexShold <ashold12@gmail.com>
2021-03-03 12:40:13 -0600

    pre refactor reviewSummary to const

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReviewList from './reviews_src/ReviewList';
+import ReviewSummary from './reviews_src/ReviewSummary';
 
 class Reviews extends React.Component {
   constructor(props) {
@@ -7,16 +8,17 @@ class Reviews extends React.Component {
     this.state = {
       reviewsData: this.props.data,
       reviewsMeta: this.props.meta,
+      currentFilters: [],
     };
   };
 
   render() {
     //console.log(this.state.reviewData.results);
+    const {reviewsData, reviewsMeta, currentFilters} = this.state;
     return (
       <div>
-        <h3>Reviews</h3>
-        {/* results is an array of reviews */}
-        <ReviewList reviews={this.state.reviewsData.results} />
+        <ReviewSummary reviewsMeta={reviewsMeta} currentFilters={currentFilters} />
+        <ReviewList reviews={reviewsData.results} />
       </div>
     );
   }
---
+++ client/src/components/reviews_src/ReviewSummary.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import StarStatic from './StarStatic';
+
+class ReviewSummary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Ratings &amp; Reviews</h3>
+      </div>
+    );
+  }
+}
+export default ReviewSummary;
