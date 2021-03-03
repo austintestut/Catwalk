2b61fb8c7753ecdf17ee3bb602132922dd1166ce
AlexShold <ashold12@gmail.com>
2021-03-03 13:18:22 -0600

    star switched to veritcal align top -> div container taken off starStatic and changed to fragement

--- client/src/components/reviews_src/ReviewSummary.jsx
+++ client/src/components/reviews_src/ReviewSummary.jsx
@@ -4,17 +4,41 @@ import StarStatic from './StarStatic';
 class ReviewSummary extends React.Component {
   constructor(props) {
     super(props);
+    let { reviewsMeta, currentFilters } = this.props;
     this.state = {
-
+      total: reviewsMeta.recommended.true + reviewsMeta.recommended.false,
+      currentFilters: currentFilters,
     };
   }
 
   render() {
+    let { reviewsMeta, currentFilters } = this.props;
+
+    const getAverage = () => {
+      let total = 0;
+      let average = 0;
+      Object.entries(reviewsMeta.ratings).forEach((review) => {
+        average += Number(review[0]) * Number(review[1]);
+        total += Number(review[1]);
+      });
+      return (average / total).toFixed(1);
+    };
+    const average = getAverage();
+    let inlineStyle = {
+      display: 'inline',
+    };
+    let starStyle = {
+      verticalAlign: 'top',
+      paddingLeft: '5px',
+    };
+
     return (
       <div>
         <h3>Ratings &amp; Reviews</h3>
+        <span><h1 style={{ ...inlineStyle }}>{average}</h1><span style={{ ...starStyle }}><StarStatic number={average}/></span></span>
       </div>
     );
   }
 }
+
 export default ReviewSummary;
