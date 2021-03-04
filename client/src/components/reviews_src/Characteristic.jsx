d57158bd1d4ed034d8d104ed105aa0d82cf7a1cc
AlexShold <ashold12@gmail.com>
2021-03-03 18:20:42 -0600

    text showing up below characteristic slider

--- client/src/components/reviews_src/Characteristic.jsx
+++ client/src/components/reviews_src/Characteristic.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
+import characteristic from '../../global_functions/characteristic';
 
-const Characteristic = ({ characteristic }) => {
+const Characteristic = ({ item }) => {
   // container width 249 px
   // each div 65 px padding auto margin
+  item = ["Size", {"id": 59528, "value": "3.2500000000000000"}];
+  const qualities = characteristic[item[0]];
   const containerStyle = {
     width: '249px',
-    height: '12px',
+    height: '18px',
   };
   const barStyle = {
     width: '80px',
     height: '12px',
     backgroundColor: 'lightGrey',
   };
+  const triangleStyle = {
+    width: 0,
+    height: 0,
+    borderLeft: '6px solid transparent',
+    borderRight: '6px solid transparent',
+    borderTop: '12px solid black',
+    position: 'absolute',
+    marginLeft: '237px', // 237 max slider left, 0 = max left
+  };
+  const textContainerStyle = {
+    justifyContent: 'space-between',
+    display: 'flex',
+    width: '249px',
+  };
+  const spanStyle = {
+    fontSize: '12px',
+  };
   return (
-    <div style={{ ...containerStyle }}>
-      <div style={{ ...barStyle, float: 'left' }} />
-      <div style={{ ...barStyle, float: 'right' }} />
-      <div style={{ ...barStyle, margin: '0 auto' }} />
+    <div>
+      <div style={{ ...containerStyle }}>
+        <div style={{ ...triangleStyle }} />
+        <div style={{ ...barStyle, float: 'left' }} />
+        <div style={{ ...barStyle, float: 'right' }} />
+        <div style={{ ...barStyle, margin: '0 auto' }} />
+      </div>
+      <div style={{ ...textContainerStyle }}>
+        <span style={{ ...spanStyle, textAlign: 'left' }}>{qualities[1]}</span>
+        <span style={{ ...spanStyle, textAlign: 'center' }}>{qualities[3]}</span>
+        <span style={{ ...spanStyle, textAlign: 'right' }}>{qualities[5]}</span>
+      </div>
     </div>
   );
 };
--- client/src/global_functions/characteristic.js
+++ client/src/global_functions/characteristic.js
@@ -1,40 +1,40 @@
 const characteristic = {
-  size: {
+  Size: {
     1: 'A size too small',
     2: '&frac12 a size too small ',
     3: 'Perfect',
     4: '&frac12 a size too big',
     5: 'A size too wide',
   },
-  width: {
+  Width: {
     1: 'Too narrow',
     2: 'Slightly Narrow',
     3: 'Perfect',
     4: 'Slightly Wide',
     5: 'Too wide',
   },
-  comfort: {
+  Comfort: {
     1: 'Uncomfortable',
     2: 'Slightly Uncomfortable',
     3: 'Ok',
     4: 'Comfortable',
     5: 'Perfect',
   },
-  quality: {
+  Quality: {
     1: 'Poor',
     2: 'Below average',
     3: 'What I expected',
     4: 'Pretty Great',
     5: 'Perfect',
   },
-  length: {
+  Length: {
     1: 'Runs Short',
     2: 'Runs slightly short',
     3: 'Perfect',
     4: 'Runs slightly long',
     5: 'Runs long',
   },
-  fit: {
+  Fit: {
     1: 'Runs tight',
     2: 'Runs slightly tight',
     3: 'Perfect',
