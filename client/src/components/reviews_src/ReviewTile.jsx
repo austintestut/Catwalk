b1c56c171719705de51ec4fd6cf1110d1b729ac8
AlexShold <ashold12@gmail.com>
2021-03-04 18:03:57 -0600

    refactor review tile to functional component

--- client/src/components/reviews_src/ReviewTile.jsx
+++ client/src/components/reviews_src/ReviewTile.jsx
@@ -8,41 +8,31 @@ import Helpfulness from './Helpfulness';
 import Response from './Response';
 import ImgModal from './ImgModal';
 
-class ReviewTile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      review: this.props.review
-    };
-  }
-
-  render() {
-    const { review } = this.props;
-    const tileStyle = {
-      marginBottom: '8px',
-      border: '2px solid grey',
-      borderRadius: '3px',
-      boxShadow: '3px 3px Grey',
-      padding: '10px',
-      paddingLeft: 0,
-    };
-    return (
-      <div style={{ ...tileStyle }}>
-        <StarStatic number={review.rating} />
-        <div>{review.reviewer_name}</div>
-        <div>{dateFormatter(review.date)}</div>
-        <Summary summary={review.summary} />
-        <Recommend bool={review.recommend} />
-        <Body body={review.body} />
-        <span>
-          { review.photos.map((img) => <ImgModal url={img.url} />) }
-        </span>
-        <ImgModal />
-        <Response response={review.response} />
-        <Helpfulness helpfulness={review.helpfulness} />
-      </div>
-    );
-  }
-}
+const ReviewTile = ({ review }) => {
+  const tileStyle = {
+    marginBottom: '8px',
+    border: '2px solid grey',
+    borderRadius: '3px',
+    boxShadow: '3px 3px Grey',
+    padding: '10px',
+    paddingLeft: 0,
+  };
+  return (
+    <div style={{ ...tileStyle }}>
+      <StarStatic number={review.rating} />
+      <div>{review.reviewer_name}</div>
+      <div>{dateFormatter(review.date)}</div>
+      <Summary summary={review.summary} />
+      <Recommend bool={review.recommend} />
+      <Body body={review.body} />
+      <span>
+        { review.photos.map((img) => <ImgModal url={img.url} />) }
+      </span>
+      <ImgModal />
+      <Response response={review.response} />
+      <Helpfulness helpfulness={review.helpfulness} />
+    </div>
+  );
+};
 
 export default ReviewTile;
