0ce3336ab70bfd062092f2dc399fdfeec62904c0
AlexShold <ashold12@gmail.com>
2021-03-08 13:13:15 -0600

    comment out review section for merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import Container from './AK/Container';
 
 const App = () => (
   <div>
-    { /* <h1>Hell World I am rendering!</h1> */}
+    <h1>Hell World I am rendering!</h1>
     {/* <Container /> */}
-    <Reviews /* productId="17764" */ />
+    { /* <Reviews /* productId="17764" */ /* />  */ }
   </div>
 );
 
