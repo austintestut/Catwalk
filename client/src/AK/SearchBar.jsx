77efba26227eaf0abf6ef5fba96d19d3675981f2
Austin Killough <austinbkillough@gmail.com>
2021-03-09 15:18:25 -0800

    Moved X button

--- client/src/AK/SearchBar.jsx
+++ client/src/AK/SearchBar.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SearchInput = styled.input`
+border: 2px solid red;
+border-width: thin;
+border-radius: 4px;
 width: 90%;
 margin: auto;
 margin-bottom: 20px;
 height: 50px;
 font-size: 20px;
+&:focus {
+  outline: bold;
+  outline-color: red;
 }
 `;
 
 const SearchBar = ({ handleSearch }) => (
-  <SearchInput placeholder="Have a question? Search for answers" onChange={handleSearch} results="0"/>
+  <SearchInput placeholder="Have a question? Search for answers" onChange={handleSearch}/>
 );
 
 export default SearchBar;
