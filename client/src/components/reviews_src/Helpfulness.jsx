6d0e0a8c44a56212b69cdedbb6de3d710ab1b10b
AlexShold <ashold12@gmail.com>
2021-03-12 12:51:15 -0600

    report new window implemented

--- client/src/components/reviews_src/Helpfulness.jsx
+++ client/src/components/reviews_src/Helpfulness.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import handler from '../../global_functions/handler';
+import UrlWindow from '../new_review_components/UrlWindow';
 
 class Helpfulness extends React.Component {
   constructor(props) {
@@ -8,6 +9,7 @@ class Helpfulness extends React.Component {
     this.state = {
       clicked: false,
       count: helpfulness,
+      report: false,
     };
     this.click = this.click.bind(this);
   }
@@ -23,8 +25,7 @@ class Helpfulness extends React.Component {
         reviews.update(id, 'helpful');
       }
       if (name === 'report') {
-        reviews.update(id, 'report');
-        this.setState({ clicked: true });
+        this.setState({ report: true });
       }
     }
   }
@@ -37,6 +38,38 @@ class Helpfulness extends React.Component {
         <span name="helpful" className="helpful austinButtons" onClick={this.click} style={{ textDecoration: 'underline' }}>Yes</span>
         <span> ({ count })    |    </span>
         <span name="report" className="helpful austinButtons" onClick = {this.click} style={{ textDecoration: 'underline' }}>Report</span>
+        {this.state.report && (
+          <UrlWindow title="FEC-Wozniak: Report Abuse">
+              <div>
+                <h1 style={{ fontFamily: 'Avenir Black' }}>Report abuse</h1>
+                <p style={{ fontFamily: 'Ariel' }}>If you find this content inappropriate
+                  and think it should be removed from the
+                  FEC-Wozniak site, let us know by clicking the button below.
+                </p>
+                <br />
+                <button
+                  type="submit"
+                  style={{
+                    marginTop: '0',
+                    backgroundColor: '#e11a2b',
+                    border: 'none',
+                    outline: '0',
+                    color: 'white',
+                    padding: '15px 32px',
+                    textAlign: 'center',
+                    textDecoration: 'none',
+                    display: 'inline-block',
+                    fontSize: '16px',
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    reviews.update(id, 'report');
+                    this.setState({ clicked: true, report: false });
+                }} >Report
+                </button>
+            </div>
+          </UrlWindow>
+        )}
       </div>
     );
   }
